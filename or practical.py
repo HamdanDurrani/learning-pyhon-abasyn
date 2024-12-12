c9d40fa67ec044beac3a005d7ec1da62ec65fdc9
HamdanDurrani <hamdanghaziofficial@gmail.com>
2024-12-12 11:19:26 +0500

    Add files via upload

---
+++ or practical.py
@@ -0,0 +1,8 @@
+# using or 
+m = input ("enter any cherter")
+if m>="a" and m<="z" or m>="A" and m<="z":
+    print ("it is alph")
+elif m>="1" and m<="9":
+    print ("it is a digit ")
+else :
+    print ("it is a special charter ") 
